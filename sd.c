3b9be99bcafb6936e458ed259ccbbe97a594e1d8
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-05-07 13:58:35 -0700

    Completed section upto part 6

--- sd.c
+++ sd.c
@@ -74,6 +74,13 @@ typedef struct
     uint32_t num_rows;
 } Table;
 
+typedef struct 
+{
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table; // Indicates a position one past the last element
+} Cursor;
+
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
@@ -334,13 +341,15 @@ void deserialize_row(void* source, Row* destination)
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-void* row_slot(Table* table, uint32_t row_num)
+void* cursor_value(Cursor* cursor)
 {
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
-
+    
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
+    
     return page + byte_offset;
 }
 
@@ -352,20 +361,24 @@ ExecuteResult execute_insert(Statement* statement, Table* table)
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+    free(cursor);
     
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement* statement, Table* table)
 {
+    Cursor* cursor = table_start(table);
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++)
+    while (!(cursor->end_of_table))
     {
-        deserialize_row(row_slot(table, i), &row);
-        print_row(&row);
+        deserialize_row(cursor_value(cursor), &row);
+        printf(&row);
+        cursor_advance(cursor);
     }
+    free(cursor);
     return EXECUTE_SUCCESS;
 }
 
@@ -380,6 +393,35 @@ ExecuteResult execute_statement(Statement* statement, Table* table)
             return execute_select(statement, table);
     }
     return EXECUTE_TABLE_FULL;
+}
+
+Cursor* table_start(Table* table)
+{
+    Cursor* cursor = malloc(sizeof(cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+CUrsor* table_end(Table* table)
+{
+    Cursor* cursor = malloc(sizeof(cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void cursor_advance(Cursor* cursor)
+{
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows)
+    {
+        cursor->end_of_table = true;
+    }
 }
 
 int main(int argc, char* argv[])
