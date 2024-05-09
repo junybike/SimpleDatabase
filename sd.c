bb2c43958f574b9234f5e49819e43787abcdf0a8
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-05-09 15:06:51 -0700

    Update: part 9

--- sd.c
+++ sd.c
@@ -460,20 +460,10 @@ Cursor* table_start(Table* table)
     return cursor;
 }
 
-Cursor* table_find(Table* table, uint32_t key)
+NodeType get_node_type(void* node)
 {
-    uint32_t root_page_num = table->root_page_num;
-    void* root_node = get_page(table->pager, root_page_num);
-
-    if (get_node_type(root_node) == NODE_LEAF)
-    {
-        return leaf_node_find(table, root_page_num, key);
-    }
-    else
-    {
-        printf("Need to implement searching an internal node.");
-        exit(EXIT_FAILURE);
-    }
+    uint8_t value = *((uint8_t*)(NODE_TYPE_OFFSET));
+    return (NodeType)value;
 }
 
 Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key)
@@ -509,10 +499,20 @@ Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key)
     return cursor;
 }
 
-NodeType get_node_type(void* node)
+Cursor* table_find(Table* table, uint32_t key)
 {
-    uint8_t value = *((uint8_t*)(NODE_TYPE_OFFSET));
-    return (NodeType)value;
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+
+    if (get_node_type(root_node) == NODE_LEAF)
+    {
+        return leaf_node_find(table, root_page_num, key);
+    }
+    else
+    {
+        printf("Need to implement searching an internal node.");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void set_node_type(void* node, NodeType type)
