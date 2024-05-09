3d4d45f7b869759929fe7a0b12e688f11f4df665
Jason Lee <79300253+junybike@users.noreply.github.com>
2024-05-09 11:30:25 -0700

    Update: part 8

--- sd.c
+++ sd.c
@@ -485,10 +485,32 @@ void cursor_advance(Cursor* cursor)
     }
 }
 
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value)
+{
+    void* node = get_page(cursor->table->pager, cursor->page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    
+    if (num_cells >= LEAF_NODE_MAX_CELLS)
+    {
+        printf("Error: Node Full. (Implement splitting a leaf node)\n");
+        exit(EXIT_FAILURE);
+    }
+    if (cursor->cell_num < num_cells)
+    {
+        for (uint32_t i = num_cells; i > cursor->cell_num; i--)
+        {
+            memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1), LEAF_NODE_CELL_SIZE);
+        }
+    }
+    *(leaf_node_num_cells(node)) += 1;
+    *(leaf_node_key(node, cursor->cell_num)) = key;
+    serialize_row(value, leaf_node_value(node, cursor->cell_num));
+}
+
 ExecuteResult execute_insert(Statement* statement, Table* table)
 {
     void* node = get_page(table->pager, table->root_page_num);
-    if (*(leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS))
+    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS))
     {
         return EXECUTE_TABLE_FULL;
     }
@@ -528,28 +550,6 @@ ExecuteResult execute_statement(Statement* statement, Table* table)
             return execute_select(statement, table);
     }
     return EXECUTE_TABLE_FULL;
-}
-
-void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value)
-{
-    void* node = get_page(cursor->table->pager, cursor->page_num);
-    uint32_t num_cells = *leaf_node_num_cells(node);
-    
-    if (num_cells >= LEAF_NODE_MAX_CELLS)
-    {
-        printf("Error: Node Full. (Implement splitting a leaf node)\n");
-        exit(EXIT_FAILURE);
-    }
-    if (cursor->cell_num < num_cells)
-    {
-        for (uint32_t i = num_cells; i > cursor->cell_num; i--)
-        {
-            memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1), LEAF_NODE_CELL_SIZE);
-        }
-    }
-    *(leaf_node_num_cells(node)) += 1;
-    *(leaf_node_key(node, cursor->cell_num)) = key;
-    serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
 
 int main(int argc, char* argv[])
